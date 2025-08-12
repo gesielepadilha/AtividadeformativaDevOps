230338b1019a73f7bece4f62a2f7e2652fac8725
Geise Padilha <gesielepadilha@gmail.com>
2025-08-12 08:19:07 -0300

    Meu primeiro commit

---
+++ AtividadeSomativaDevOps/AtividadeSomativaDevOps/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AtividadeSomativaDevOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando a Formativa de DevOps!");
+
+        }
+    }
+}
